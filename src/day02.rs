60146466d9079ed49172d429254e4d2932e21133
Callum Oakley <c.oakley108@gmail.com>
2018-12-02 12:14:14 +0000

    reduce unnecessary work

--- src/day02.rs
+++ src/day02.rs
@@ -3,54 +3,52 @@ use std::collections::HashMap;
 
 pub fn part1(input: &str) -> Result<usize> {
     let ids = parse_input(input)?;
-    let contains2 = ids.iter().filter(|id| contains_repeats(2, id)).count();
-    let contains3 = ids.iter().filter(|id| contains_repeats(3, id)).count();
-    Ok(contains2 * contains3)
+
+    let mut twos = 0;
+    let mut threes = 0;
+
+    for id in ids {
+        let mut frequencies = HashMap::new();
+        for c in id.chars() {
+            frequencies.entry(c).and_modify(|f| *f += 1).or_insert(1);
+        }
+        if frequencies.values().any(|f| *f == 2) {
+            twos += 1
+        }
+        if frequencies.values().any(|f| *f == 3) {
+            threes += 1
+        }
+    }
+
+    Ok(twos * threes)
 }
 
 pub fn part2(input: &str) -> Result<String> {
     let ids = parse_input(input)?;
 
-    for a in ids.iter() {
-        for b in ids.iter() {
-            let common = common_letters(a, b);
-            if common.misses == 1 {
-                return Ok(common.letters);
+    for (i, a) in ids.iter().enumerate() {
+        for b in ids.iter().skip(i + 1) {
+            let mut common = String::new();
+            let mut misses = 0;
+
+            for (a, b) in a.chars().zip(b.chars()) {
+                if a == b {
+                    common.push(a);
+                } else {
+                    misses += 1;
+                    if misses > 1 {
+                        break;
+                    }
+                }
+            }
+
+            if misses == 1 {
+                return Ok(common);
             }
         }
     }
 
     bail!("couldn't find the prototype boxes");
-}
-
-fn contains_repeats(n: i32, id: &str) -> bool {
-    let mut frequencies = HashMap::new();
-    for c in id.chars() {
-        frequencies.entry(c).and_modify(|f| *f += 1).or_insert(1);
-    }
-    frequencies.values().any(|f| *f == n)
-}
-
-struct CommonLetters {
-    letters: String,
-    misses: i32,
-}
-
-fn common_letters(a: &str, b: &str) -> CommonLetters {
-    let mut res = CommonLetters {
-        letters: String::new(),
-        misses: 0,
-    };
-
-    for (a, b) in a.chars().zip(b.chars()) {
-        if a == b {
-            res.letters.push(a);
-        } else {
-            res.misses += 1;
-        }
-    }
-
-    res
 }
 
 fn parse_input(input: &str) -> Result<Vec<&str>> {
